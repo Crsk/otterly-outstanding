799252f059f124fe35000206edeb6543a94f9997
Christopher Kiessling <Crsk@mail.com>
2023-01-25 02:48:29 -0300

    refactor: initialize server from core module and add validation schema

--- core/src/lib/core.module.ts
+++ core/src/lib/core.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config";
+import { configuration } from "./config/configuration";
+import { validationSchema } from "./config/validation";
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validationSchema
+    })
+  ],
   controllers: [],
   providers: [],
-  exports: [],
+  exports: []
 })
-export class CoreModule {}
+
+export class CoreModule { }
