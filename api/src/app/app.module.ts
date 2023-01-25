36b36abb6265b2aac0c33bab0515134ed9debfb7
Christopher Kiessling <Crsk@mail.com>
2023-01-25 04:30:07 -0300

    fix(merge): recover configuration code lost

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoreModule } from 'core/src';
+import { CoreModule } from '@otterly-outstanding/core';
 
 @Module({
   imports: [CoreModule],
